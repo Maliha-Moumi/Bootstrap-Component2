9c1f034e9487e2a94a44b1cbe3981765d75242d5
Maliha Moumi <maliha.moumii@gmail.com>
2022-06-05 13:29:11 +0600

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 const toastTrigger = document.getElementById('liveToastBtn')
